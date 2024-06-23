12741c4d34ba18b779df56caee5acf87d3b5d1da
Siddart007 <129648938+Siddart007@users.noreply.github.com>
2024-06-23 12:59:51 +0530

    Add files via upload

---
+++ db/index.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+// Connect to MongoDB by putting your url here.
+mongoose.connect('mongodb+srv://admin:dU54ek6tD!VQWPv@cluster0.kv26vq1.mongodb.net/hellotable');
+
+const AdminSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
+});
+
+const CourseSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    imageLink: String,
+    price: Number
+});
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
+
+module.exports = {
+    Admin,
+    User,
+    Course
+}
